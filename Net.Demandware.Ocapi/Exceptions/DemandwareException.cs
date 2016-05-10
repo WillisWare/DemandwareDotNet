f8547529d720b855c51620d96725467b068f2511
jowillis <jonathan.willis@newbalance.com>
2016-05-10 16:19:57 -0500

    Added app configs to projects, but files are still ignored by Git.

--- Net.Demandware.Ocapi/Exceptions/DemandwareException.cs
+++ Net.Demandware.Ocapi/Exceptions/DemandwareException.cs
@@ -3,13 +3,22 @@ using Newtonsoft.Json;
 
 namespace Net.Demandware.Ocapi.Exceptions
 {
+    /// <summary>
+    /// Exception class representing the structure and content of an error response returned from a Demandware API call.
+    /// </summary>
     public sealed class DemandwareException
     {
         #region Properties
 
+        /// <summary>
+        /// Gets or sets the Fault document that defines the error.
+        /// </summary>
         [JsonProperty("fault")]
         public Fault Fault { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Demandware instance version that threw the error.
+        /// </summary>
         [JsonProperty("_v")]
         public string Version { get; set; }
 
